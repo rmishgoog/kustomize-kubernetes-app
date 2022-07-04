13f92f2ebd819e578853abb0408ec59826869945
Rohit Mishra <rohitmishra@google.com>
2022-07-04 21:57:10 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+##### Sample application for GitOps demonstration with Google Cloud Build and Argo CD
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type car struct {
+	ID     string  `json:"id"`
+	Name   string  `json:"name"`
+	Stock  int     `json:"stock"`
+	Price  float64 `json:"price"`
+	Origin string  `json:"origin"`
+}
+
+var cars = []car{
+	{ID: "1", Name: "Ford Mustang GT 5.0", Stock: 100, Price: 55000.99, Origin: "USA"},
+	{ID: "2", Name: "Ford Explorer ST", Stock: 55, Price: 60000.99, Origin: "USA"},
+	{ID: "4", Name: "Ford Escape Compact", Stock: 10, Price: 32000.89, Origin: "USA"},
+	{ID: "5", Name: "Ford Edge LX Luxury", Stock: 10, Price: 38000.89, Origin: "Germany"},
+	{ID: "6", Name: "Ford Edge LX Base", Stock: 10, Price: 36000.89, Origin: "Germany"},
+	{ID: "7", Name: "Ford Edge LX Base 2022", Stock: 10, Price: 36000.89, Origin: "Germany"},
+}
+
+func getCars(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, cars)
+}
+
+func addCars(c *gin.Context) {
+	var newCar car
+	if err := c.BindJSON(&newCar); err != nil {
+		return
+	}
+	cars = append(cars, newCar)
+	c.IndentedJSON(http.StatusCreated, newCar)
+}
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.GET("/cars", getCars)
+	router.POST("/cars", addCars)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+		log.Printf("main.go: defaulting to port %s", port)
+	}
+	log.Printf("listening on port %s", port)
+	router.Run(":" + port + "")
+}
